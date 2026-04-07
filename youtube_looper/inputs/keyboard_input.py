64f713b9490b127effe582ff61412f78e4955e8f
Shane Ciardelli <135490943+dgshanee@users.noreply.github.com>
2026-04-07 13:27:59 -0500

    error handling on keyboard input

--- youtube_looper/inputs/keyboard_input.py
+++ youtube_looper/inputs/keyboard_input.py
@@ -1,12 +1,14 @@
 from pynput import keyboard
 import ctypes
 
-
 def start_keyboard_tracking(shmem, lib):
     lib.toggle_playback.argtypes = [ctypes.c_void_p]
     def on_press(key):
-        if key.char == 'p':
-            lib.toggle_playback(shmem)
+        try:
+            if key.char == 'p':
+                lib.toggle_playback(shmem)
+        except AttributeError:
+            pass
 
     listener = keyboard.Listener(on_press=on_press)
     listener.start()
